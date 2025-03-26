b2a6ea034018732f362488b9a08554243c0d1307
bbro837 <bbro837@wgu.edu>
2025-03-26 00:05:57 +0000

    path update

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "/home/bbro837/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = "/workspace/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/data/census.csv"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
